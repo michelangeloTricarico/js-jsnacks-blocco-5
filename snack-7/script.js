c5d59c142c8b11ca6ec325f792397a3fd4ff0f5a
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-03-28 18:40:01 +0100

    snack7 finalization

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+// Method find to find first student with id == 2
+
+const student = students.find(element => element.id == 2)
+console.log(student) //print result
