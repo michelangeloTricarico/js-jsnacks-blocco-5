656d3f803be4686f1d0f85b295fa89f2c756659b
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-03-28 18:03:06 +0100

    snack1 finalization

---
+++ snack-1/script.js
@@ -0,0 +1,7 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// Method forEach with arrow function
+names.forEach(name => console.log(name))
