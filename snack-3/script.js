21132e42c619a06d5541328616f5d859c76b5400
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-03-28 18:13:50 +0100

    snack3 finalization

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,9 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+// Method map to cycle inside list and define a new array with values increased by 1
+
+const numbers_increased = numbers.map(element => element + 1)
+console.log(numbers_increased) // print results
+
