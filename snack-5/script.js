a95e5a317a119096e4231f0b8668bdbfb657a93c
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-03-28 18:28:18 +0100

    snack5 finalization

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,9 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+// Method filter to create an array with only even numbers
+
+let even_numbers = [] //new list initialization
+nums.filter( element => {if (element % 2 == 0){even_numbers.push(element)}}) // filter
+console.log(even_numbers) //print results
